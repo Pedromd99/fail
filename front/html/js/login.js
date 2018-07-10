aaca52c8d4c64af0f832698523ae105942ecd86b
Pedromd99 <pmoradodiaz@gmail.com>
2018-07-10 10:05:33 +0200

    mi primer commit

--- front/html/js/login.js
+++ front/html/js/login.js
@@ -1,27 +1,30 @@
-var vm = new Vue ({
+var vm = new Vue({
   el: '#app',
   data: {
     loading: false,
-    username:"",
-    password:"",
+    username: "",
+    password: "",
+    // token:""
   },
   methods: {
-    postapi: function (data) {
-      var usus = (this.username);
+    postapi: function() {
+      var user = (this.username);
       var pass = (this.password);
-      console.log(usus, pass);
+      console.log(user, pass);
       this.loading = true;
-      this.$http.post('http://127.0.0.1:8000/api-token-auth/',{username:usus, password:pass})
-      .then((reponse) =>{
-          localStorage.token = data.token;
-          console.log(data.token);
-          // window.location.replace("tareas.html");
+      this.$http.post('http://127.0.0.1:8000/api-token-auth/', {username: user, password: pass})
+      .then((response) => {
+        this.loading = false;
+        // this.commit('token', response.data.token);
+        localStorage.token = response.data.token;
+        console.log(response);
+        window.location.replace("tareas.html");
       })
       .catch((err) => {
         this.loading = false;
         console.log(err);
+        console.log('Fail');
       })
     },
-  }
-
+  },
 })
--- front/html/js/tareas.js
+++ front/html/js/tareas.js
@@ -1,24 +1,73 @@
 var vm = new Vue({
-el: '#app',
+  el: '#app',
   data: {
-    tasks: [],
-    taskstodelete: [],
-    loading: false,
-    message: null,
-    new_task: {
+    Tareas: [],
+    delTareas: [],
+    currentTarea: {},
+    newTarea: {
       'description': null,
       'pending': true
     },
+    loading: false,
+    message: null,
+    token: localStorage.token,
   },
   mounted: function() {
-    this.getArticles();
+    this.verify();
+    this.getTareas();
   },
   methods: {
-    getArticles: function() {
+    verify: function() {
       this.loading = true;
-      this.$http.get('http://127.0.0.1:8000/')
+      this.$http.post('http://127.0.0.1:8000/api-token-verify/', {
+          token: localStorage.token
+        })
+        .then((response) => {
+          console.log('OK');
+          localStorage.token = response.data.token;
+          // this.tareas = response.data;
+          this.loading = false;
+        })
+        .catch((err) => {
+          this.loading = false;
+          window.location.replace("/");
+          console.log(err);
+        })
+    },
+    getTareas: function() {
+      // console.log();
+      this.loading = true;
+      this.$http.get('http://127.0.0.1:8000/tareas/', {
+          headers: {
+            Authorization: "Token " + (localStorage.token),
+          },
+        })
+        .then((response) => {
+          // console.log(response);
+
+          this.Tareas = response.data.results;
+          this.loading = false;
+        })
+        .catch((err) => {
+          this.loading = false;
+          console.log(err);
+        })
+
+    },
+
+    getTarea: function(id) {
+      console.log(id);
+      this.loading = true;
+      this.$http.get('http://127.0.0.1:8000/tareas/' + id + '/', {
+          headers: {
+            "Authorization": "Token " + (localStorage.token),
+          },
+        })
         .then((response) => {
 
+          this.currentTarea = response.data;
+          console.log(this.currentTarea);
+          $("#edit").modal('show');
           this.loading = false;
         })
         .catch((err) => {
@@ -26,55 +75,109 @@ el: '#app',
           console.log(err);
         })
     },
-    getArticle: function(id) {
+    addTarea: function() {
       this.loading = true;
-      this.$http.get()
+      this.$http.post('http://127.0.0.1:8000/tareas/', this.newTarea, {
+          headers: {
+            "Authorization": "Token " + (localStorage.token),
+          },
+        })
         .then((response) => {
+          this.loading = false;
+          this.getTareas();
+        })
+        .catch((err) => {
+          this.loading = false;
+          console.log(err);
+        })
+    },
+    updateTarea: function(id) {
+      // console.log(this.id);
+      this.loading = true;
+      this.$http.put('http://127.0.0.1:8000/tareas/' + id + '/', this.currentTarea, {
+          headers: {
+            "Authorization": "Token " + (localStorage.token),
+          },
+        })
+        .then((response) => {
+          this.loading = false;
+          this.getTareas();
+        })
+        .catch((err) => {
+          this.loading = false;
+          console.log(err);
+        })
+    },
+    deleteTarea: function(id) {
+      this.loading = true;
+      this.$http.delete('http://127.0.0.1:8000/tareas/' + id + '/', {
+          headers: {
+            "Authorization": "Token " + (localStorage.token),
+          },
+        })
+        .then((response) => {
+          this.loading = false;
+          this.getTareas();
+        })
+        .catch((err) => {
+          this.loading = false;
+          console.log(err);
+        })
+    },
 
-          this.loading = false;
-        })
-        .catch((err) => {
-          this.loading = false;
-          console.log(err);
-        })
+    marcar: function(id) {
+      this.Tareas.pending = !this.Tareas.pending;
+      console.log(this.Tareas.pending);
+      if (this.Tareas.pending) {
+        this.loading = true;
+        this.$http.put('http://127.0.0.1:8000/tareas/' + id + '/', this.Tareas, {
+            headers: {
+              "Authorization": "Token " + (localStorage.token),
+            },
+          })
+          .then((response) => {
+            this.loading = false;
+            this.delTareas = response.data;
+            console.log(this.Tareas.description);
+          })
+          .catch((err) => {
+            this.loading = false;
+            console.log(err);
+          })
+      } else {
+        this.loading = true;
+        this.$http.put('http://127.0.0.1:8000/tareas/' + id + '/', this.Tareas, {
+            headers: {
+              "Authorization": "Token " + (localStorage.token),
+            },
+          })
+          .then((response) => {
+            this.loading = false;
+            this.delTareas = response.data;
+            console.log(this.Tareas.description);
+          })
+          .catch((err) => {
+            this.loading = false;
+            console.log(err);
+          })
+      }
     },
-    addArticle: function() {
-      this.loading = true;
-      this.$http.post()
-        .then((response) => {
 
-          this.getArticles();
-        })
-        .catch((err) => {
-          this.loading = false;
-          console.log(err);
-        })
-    },
-    updateArticle: function() {
+    deleteTareas: function(id) {
       this.loading = true;
-      this.$http.put()
+      this.$http.delete('http://127.0.0.1:8000/tareas/' + id + '/', {
+          headers: {
+            "Authorization": "Token " + (localStorage.token),
+          },
+        })
         .then((response) => {
           this.loading = false;
-        
-          this.getArticles();
-        })
-        .catch((err) => {
-          this.loading = false;
-          console.log(err);
-        })
-    },
-    deleteArticle: function(id) {
-      this.loading = true;
-      this.$http.delete()
-        .then((response) => {
-          this.loading = false;
-          this.getArticles();
+          this.getTareas();
         })
         .catch((err) => {
           this.loading = false;
           console.log(err);
         })
     }
-  }
-
+  },
 });
